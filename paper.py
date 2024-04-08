3ccf91669afdf69d017feadd182543e55543d59f
Katesigned <166055430+Katesigned@users.noreply.github.com>
2024-04-08 17:06:31 +0100

    Update paper.py

--- paper.py
+++ paper.py
@@ -52,8 +52,8 @@ _URL = "https://rajpurkar.github.io/SQuAD-explorer/dataset/"
 _URLS = {
  #  "train":_URL +"train-v1.1.json",
     #  "dev": "https://raw.githubusercontent.com/katesigned/paper/dev-1.1.json",
-     "dev"https://huggingface.co/Katesignedup/paper/edit/main/dev-1.1.csv",
-     "https://huggingface.co/Katesignedup/paper/raw/main/dev-1.1.csv",
+    #  "dev"https://huggingface.co/Katesignedup/paper/edit/main/dev-1.1.csv",
+     "https://huggingface.co/Katesignedup/paper/raw/main/dev-v1.1.json",
 }
 
 
